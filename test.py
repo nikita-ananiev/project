1ae61191872f26f509415f123b28d3b1092ea76b
Nikita Ananiev <nickita-ananiev@yandex.ru>
2019-11-20 22:55:34 +0300

    Add test.py

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-a = input()
-print("hi man", a)
+import pygame
+# инициализация Pygame:
+pygame.init()
+# размеры окна:
+
+# screen — холст, на котором нужно рисовать:
+screen = pygame.display.set_mode((500, 500))
+
+
+fps = 120
+clock = pygame.time.Clock()
+screen.fill((250, 0, 0))
+pygame.draw.circle(screen, pygame.Color('white'), (0, 0), 100)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
